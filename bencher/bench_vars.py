ba25a0fe2263786bc28bfdc58f86bc2099294c31
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-06-10 13:57:13 +0000

    temporarily skip bench_vars file for linting

--- bencher/bench_vars.py
+++ bencher/bench_vars.py
@@ -1,8 +1,9 @@
+# pylint: skip-file
+from param import Parameterized, Number, Selector, Integer, Boolean
+import param
 from enum import Enum, auto
 from strenum import StrEnum
-
 import numpy as np
-import param
 from pandas import Timestamp
 import re
 from datetime import datetime
@@ -35,13 +36,11 @@ def un_camel(camel: str) -> str:
     return capitalise_words(re.sub("([a-z])([A-Z])", r"\g<1> \g<2>", camel.replace("_", " ")))
 
 
-def param_hash(
-    param_type: param.Parameterized, hash_value: bool = True, hash_meta: bool = False
-) -> int:
+def param_hash(param_type: Parameterized, hash_value: bool = True, hash_meta: bool = False) -> int:
     """A custom hash function for parametrised types with options for hashing the value of the type and hashing metadata
 
     Args:
-        param_type (param.Parameterized): A parameter
+        param_type (Parameterized): A parameter
         hash_value (bool, optional): use the value as part of the hash. Defaults to True.
         hash_meta (bool, optional): use metadata as part of the hash. Defaults to False.
 
@@ -64,14 +63,14 @@ def param_hash(
     return curhash
 
 
-class ParametrizedSweep(param.Parameterized):
+class ParametrizedSweep(Parameterized):
     """Parent class for all Sweep types that need a custom hash"""
 
     def __hash__(self) -> int:
         return param_hash(self, True, False)
 
 
-class ParametrizedOutput(param.Parameterized):
+class ParametrizedOutput(Parameterized):
     """Parent class for all Output types that need a custom hash"""
 
     def __hash__(self) -> int:
@@ -83,7 +82,7 @@ class ParametrizedOutput(param.Parameterized):
 shared_slots = ["units", "samples", "samples_debug"]
 
 
-def sweep_hash(parameter: param.Parameterized) -> int:
+def sweep_hash(parameter: Parameterized) -> int:
     """Generate a hash for a sweep variable
 
     Returns:
@@ -96,11 +95,11 @@ def sweep_hash(parameter: param.Parameterized) -> int:
     return curhash
 
 
-def hash_extra_vars(parameter: param.Parameterized) -> int:
+def hash_extra_vars(parameter: Parameterized) -> int:
     """hash extra meta vars in the parameter
 
     Args:
-        parameter (param.Parameterized): a parameter
+        parameter (Parameterized): a parameter
 
     Returns:
         int: hash
@@ -109,21 +108,20 @@ def hash_extra_vars(parameter: param.Parameterized) -> int:
 
 
 def describe_variable(
-    v: param.Parameterized, debug: bool, include_samples: bool, indent_count=1
+    v: Parameterized, debug: bool, include_samples: bool, indent_count=1
 ) -> List[str]:
     """Generate a string description of a variable
 
     Args:
-        v (param.param.Parameterized): parameter to describe
-        debug (bool): Generate a reduced param.Number of samples from the variable
+        v (param.Parameterized): parameter to describe
+        debug (bool): Generate a reduced number of samples from the variable
         include_samples (bool): Include a description of the samples
-        indent_count (int, optional): param.Number of times to indent the string. Defaults to 1.
+        indent_count (int, optional): Number of times to indent the string. Defaults to 1.
 
     Returns:
         str: String description of the variable
     """
     indent = "    "
-    indent *= indent_count
     sampling_str = []
     sampling_str.append(f"{v.name}:")
     if include_samples:
@@ -132,17 +130,18 @@ def describe_variable(
     if v.doc is not None:
         sampling_str.append(f"{indent}docs: {v.doc}")
     for i in range(len(sampling_str)):
-        sampling_str[i] = f"{indent}{sampling_str[i]}"
+        for c in range(indent_count):
+            sampling_str[i] = f"{indent}{sampling_str[i]}"
     return sampling_str
 
 
-class BoolSweep(param.Boolean):
-    """A class to represent a parameter sweep of bools"""
+class BoolSweep(Boolean):
+    """A class to reprsent a parameter sweep of bools"""
 
     __slots__ = shared_slots
 
     def __init__(self, units: str = "ul", samples: int = None, samples_debug: int = 2, **params):
-        param.Boolean.__init__(self, **params)
+        Boolean.__init__(self, **params)
         self.units = units
         if samples is None:
             self.samples = 2
@@ -155,14 +154,13 @@ class BoolSweep(param.Boolean):
 
     def sampling_str(self, debug: bool) -> str:
         """Generate a string representation of the sampling procedure"""
-        print(self.sampling_str(debug))
         return f"sampling {self.name} from: [True,False]"
 
     def __hash__(self) -> int:
         return hash_extra_vars(self)
 
 
-class TimeBase(param.Selector):
+class TimeBase(Selector):
     """A class to capture a time snapshot of benchmark values.  Time is reprented as a continous value i.e a datetime which is converted into a np.datetime64.  To represent time as a discrete value use the TimeEvent class. The distinction is because holoview and plotly code makes different assumptions about discrete vs continous variables"""
 
     __slots__ = shared_slots
@@ -178,7 +176,6 @@ class TimeBase(param.Selector):
         Args:
             debug (bool): If true then self.samples_debug is used
         """
-        print(self.sampling_str(debug))
         return f"sampling from [The Past to {self.objects[0]}]"
 
     def __hash__(self) -> int:
@@ -198,11 +195,10 @@ class TimeSnapshot(TimeBase):
         samples_debug: int = 2,
         **params,
     ):
-        super().__init__()
         if type(datetime_src) == str:
-            param.Selector.__init__(self, [datetime_src], instantiate=True, **params)
+            Selector.__init__(self, [datetime_src], instantiate=True, **params)
         else:
-            param.Selector.__init__(
+            Selector.__init__(
                 self,
                 [Timestamp(datetime_src)],
                 instantiate=True,
@@ -217,7 +213,7 @@ class TimeSnapshot(TimeBase):
 
 
 class TimeEvent(TimeBase):
-    """A class to represent a discrete event in time where the data was captured i.e a series of pull requests.  Here time is discrete and can't be interpolated, to represent time as a continous value use the TimeSnapshot class.  The distinction is because holoview and plotly code make different assumptions about discrete vs continous variables"""
+    """A class to represent a discrete event in time where the data was captured i.e a series of pull requests.  Here time is discrete and can't be interpolated, to represent time as a continous value use the TimeSnapshot class.  The distinction is because holoview and plotly code makes different assumptions about discrete vs continous variables"""
 
     __slots__ = shared_slots
 
@@ -229,8 +225,7 @@ class TimeEvent(TimeBase):
         samples_debug: int = 2,
         **params,
     ):
-        super().__init__()
-        param.Selector.__init__(
+        Selector.__init__(
             self,
             [time_event],
             instantiate=True,
@@ -244,7 +239,7 @@ class TimeEvent(TimeBase):
         self.samples_debug = min(self.samples, samples_debug)
 
 
-class StringSweep(param.Selector):
+class StringSweep(Selector):
     """A class to reprsent a parameter sweep of strings"""
 
     __slots__ = shared_slots
@@ -257,7 +252,7 @@ class StringSweep(param.Selector):
         samples_debug: int = 2,
         **params,
     ):
-        param.Selector.__init__(self, string_list, instantiate=True, **params)
+        Selector.__init__(self, string_list, instantiate=True, **params)
         self.units = units
         if samples is None:
             self.samples = len(self.objects)
@@ -277,34 +272,28 @@ class StringSweep(param.Selector):
             debug (bool): If true then self.samples_debug is used
         """
         object_str = ",".join([i for i in self.objects])
-        print(self.sampling_str(debug))
         return f"sampling {self.name} from: [{object_str}]"
 
     def __hash__(self) -> int:
         return hash_extra_vars(self)
 
 
-class EnumSweep(param.Selector):
+class EnumSweep(Selector):
     """A class to reprsent a parameter sweep of enums"""
 
     __slots__ = shared_slots
 
     def __init__(
-        self,
-        enum_type: Enum | List[Enum],
-        units="ul",
-        samples=None,
-        samples_debug=2,
-        **params,
+        self, enum_type: Enum | List[Enum], units="ul", samples=None, samples_debug=2, **params
     ):
         # The enum can either be an Enum type or a list of enums
         list_of_enums = type(enum_type) is list
         if list_of_enums:
-            param.Selector_list = enum_type  # already a list of enums
+            selector_list = enum_type  # already a list of enums
         else:
             # create a list of enums from the enum type definition
-            param.Selector_list = [e for e in enum_type]
-        param.Selector.__init__(self, param.Selector_list, **params)
+            selector_list = [e for e in enum_type]
+        Selector.__init__(self, selector_list, **params)
         if not list_of_enums:  # Grab the docs from the enum type def
             self.doc = enum_type.__doc__
         self.units = units
@@ -316,10 +305,8 @@ class EnumSweep(param.Selector):
 
     def values(self, debug) -> List[Enum]:
         """return all the values for a parameter sweep.  If debug is true return a reduced list"""
-        if debug:
-            return [self.objects[0], self.objects[-1]]
-        else:
-            return self.objects
+        print(self.sampling_str(debug))
+        return self.objects
 
     def sampling_str(self, debug: bool) -> str:
         """Generate a string representation of the of the sampling procedure
@@ -327,7 +314,7 @@ class EnumSweep(param.Selector):
         Args:
             debug (bool): If true then self.samples_debug is used
         """
-        object_str = ",".join([i for i in self.values(debug)])
+        object_str = ",".join([i for i in self.objects])
         return f"sampling {self.name} from: [{object_str}]"
 
     def __hash__(self) -> int:
@@ -344,20 +331,20 @@ def int_float_sampling_str(name, samples) -> str:
     return f"sampling {name} from {samples} in {len(samples)} samples"
 
 
-class IntSweep(param.Integer):
-    """A class to represent a parameter sweep of ints"""
+class IntSweep(Integer):
+    """A class to reprsent a parameter sweep of ints"""
 
     __slots__ = shared_slots + ["sample_values"]
 
     def __init__(self, units="ul", samples=None, samples_debug=2, sample_values=None, **params):
-        param.Integer.__init__(self, **params)
+        Integer.__init__(self, **params)
         self.units = units
         self.samples_debug = samples_debug
 
         if sample_values is None:
             if samples is None:
                 if self.bounds is None:
-                    raise RuntimeError("You must define bounds for param.Integer types")
+                    raise RuntimeError("You must define bounds for integer types")
                 else:
                     self.samples = self.bounds[1] - self.bounds[0]
             else:
@@ -399,13 +386,13 @@ class IntSweep(param.Integer):
         return hash_extra_vars(self)
 
 
-class FloatSweep(param.Number):
+class FloatSweep(Number):
     """A class to represent a parameter sweep of floats"""
 
     __slots__ = shared_slots + ["sample_values"]
 
     def __init__(self, units="ul", samples=10, samples_debug=2, sample_values=None, **params):
-        param.Number.__init__(self, **params)
+        Number.__init__(self, **params)
         self.units = units
         self.samples_debug = samples_debug
         if sample_values is None:
@@ -452,13 +439,13 @@ class OptDir(StrEnum):
     none = auto()  # If none this var will not appear in pareto plots
 
 
-class ResultVar(param.Number):
+class ResultVar(Number):
     """A class to represent result variables and the desired optimisation direction"""
 
     __slots__ = ["units", "direction"]
 
     def __init__(self, units="ul", direction: OptDir = OptDir.minimize, **params):
-        param.Number.__init__(self, **params)
+        Number.__init__(self, **params)
         self.units = units
         self.default = 0  # json is terrible and does not support nan values
         self.direction = direction
