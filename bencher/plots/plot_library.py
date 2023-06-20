c82233a112825bb4474d1fd7bb64d78a3ef5c414
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-06-20 17:55:05 +0000

    document plot_library.py

--- bencher/plots/plot_library.py
+++ bencher/plots/plot_library.py
@@ -7,10 +7,7 @@ from enum import auto
 
 
 class AllPlotTypes(StrEnum):
-    # table enums
-    dataframe_raw = auto()
-    dataframe_mean = auto()
-    xarray = auto()
+    """A enum that contains all of the inbuilt plot types.  The enum is a strEnum so the value of the enum is a str representation of the name of itself.  e.g AllPlotTypes.swarmplot is equal to the string 'swarmplot'.  The PlotCollection class has an add() and remove() method that takes the string name of the plot to add or remove.  You can either pass raw string, or to help reduce typos and enhance discoverability use AllPlottypes.xxxxxxxx."""
 
     # catplot enums
     swarmplot = auto()
@@ -18,10 +15,23 @@ class AllPlotTypes(StrEnum):
     violinplot = auto()
     boxenplot = auto()
 
+    # table enums
+    dataframe_multi_index = auto()
+    dataframe_flat = auto()
+    dataframe_mean = auto()
+    xarray = auto()
+
 
 class PlotLibrary:
+    """This class is a collection of static factory methods that define sets of plots.  It provides a sensible default PlotCollection out of the box but also enables the creation of custom PlotCollections"""
+
     @staticmethod
-    def setup_sources():
+    def setup_sources() -> PlotCollection:
+        """Shared method that collects all the possible plot types.  This is used by all the other methods that select different subsections of the plots
+
+        Returns:
+            PlotCollection: A PlotCollection with sources of plots defined, but none selected
+        """
         plt_col = PlotCollection()
         plt_col.add_plotter_source(Catplot())
         plt_col.add_plotter_source(Tables())
@@ -29,6 +39,35 @@ class PlotLibrary:
 
     @staticmethod
     def default() -> PlotCollection:
+        """Default PlotCollection.  This set of plots should provide a reasonable set of plots that cover the most common data plotting use cases
+
+        Returns:
+            PlotCollection: sensible default for PlotCollection
+        """
         plt_col = PlotLibrary.setup_sources()
         plt_col.add(AllPlotTypes.swarmplot)
+        return plt_col
+
+    @staticmethod
+    def tables() -> PlotCollection:
+        """Display only table summaries of the data
+
+        Returns:
+            PlotCollection: Only table plots
+        """
+        plt_col = PlotLibrary.setup_sources()
+        plt_col.add(AllPlotTypes.dataframe_multi_index)
+        plt_col.add(AllPlotTypes.dataframe_mean)
+        return plt_col
+
+    @staticmethod
+    def all() -> PlotCollection:
+        """Display the data in every possible way. All plots are included
+
+        Returns:
+            PlotCollection: All possible plots
+        """
+        plt_col = PlotLibrary.setup_sources()
+        for pt in [p for p in AllPlotTypes]:  # iterate through all plot enums
+            plt_col.add(pt)
         return plt_col
