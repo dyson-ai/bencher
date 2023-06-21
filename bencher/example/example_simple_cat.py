978d27761165f5ef0b70ff96b94d0643b878e7fd
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-06-21 18:11:17 +0000

    revert to origianl cat example

--- bencher/example/example_simple_cat.py
+++ bencher/example/example_simple_cat.py
@@ -1,12 +1,12 @@
 """This file has some examples for how to perform basic benchmarking parameter sweeps"""
 
-import bencher as bch
+from bencher.bencher import Bench, BenchRunCfg
 
 # All the examples will be using the data structures and benchmark function defined in this file
 from bencher.example.benchmark_data import ExampleBenchCfgIn, ExampleBenchCfgOut, bench_function
 
 
-def example_1D_cat(run_cfg: bch.BenchRunCfg) -> bch.Bench:
+def example_1D_cat(run_cfg: BenchRunCfg) -> Bench:
     """This example shows how to sample a 1 dimensional categorical variable and plot the result of passing that parameter sweep to the benchmarking function
 
     Args:
@@ -16,16 +16,7 @@ def example_1D_cat(run_cfg: bch.BenchRunCfg) -> bch.Bench:
         Bench: results of the parameter sweep
     """
 
-    bencher = bch.Bench("benchmarking_example_categorical1D", bench_function, ExampleBenchCfgIn)
-
-    plot_lib = bch.PlotLibrary.default()
-    plot_lib.add(bch.PlotTypes.barplot)
-    plot_lib.add(bch.PlotTypes.dataframe_flat)
-    plot_lib.add(bch.PlotTypes.dataframe_mean)
-
-    plot_lib.remove(bch.PlotTypes.swarmplot)
-
-    plot_lib = bch.PlotLibrary.all()
+    bencher = Bench("benchmarking_example_categorical1D", bench_function, ExampleBenchCfgIn)
 
     # here we sample the input variable theta and plot the value of output1. The (noisy) function is sampled 20 times so you can see the distribution
     bencher.plot_sweep(
@@ -34,15 +25,14 @@ def example_1D_cat(run_cfg: bch.BenchRunCfg) -> bch.Bench:
         result_vars=[ExampleBenchCfgOut.param.out_cos, ExampleBenchCfgOut.param.out_sin],
         description=example_1D_cat.__doc__,
         run_cfg=run_cfg,
-        plot_lib=plot_lib,
     )
     return bencher
 
 
 if __name__ == "__main__":
-    ex_run_cfg = bch.BenchRunCfg()
+    ex_run_cfg = BenchRunCfg()
     ex_run_cfg.repeats = 10
     ex_run_cfg.print_pandas = True
-    # ex_run_cfg.over_time = True
+    ex_run_cfg.over_time = True
 
     example_1D_cat(ex_run_cfg).plot()
