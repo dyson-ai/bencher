1f40544039ab329077b15435039b56ba88a9e9b4
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-06-21 18:02:39 +0000

    update the tables class

--- bencher/plotting/plots/tables.py
+++ bencher/plotting/plots/tables.py
@@ -4,37 +4,27 @@ from bencher.bench_vars import ParametrizedSweep
 
 from typing import List
 import panel as pn
+from bencher.plotting.plot_types import AllPlotTypes
 
 
 class Tables:
     """A class to display the result data in tabular form"""
 
-    match_all = PlotFilter(
-        float_range=VarRange(None, None),
-        cat_range=VarRange(None, None),
-        vector_len=VarRange(None, None),
-        result_vars=VarRange(None, None),
-    )
-
     def dataframe_flat(self, pl_in: PlotInput) -> List[pn.panel]:
-        if self.match_all.matches(pl_in.plt_cnt_cfg):
-            df = pl_in.bench_cfg.get_dataframe()
-            return [pn.pane.DataFrame(df, name=self.dataframe_flat.__name__)]
-        return []
+        """Returns a list of panel objects containing a flat dataframe."""
+        df = pl_in.bench_cfg.get_dataframe()
+        return [pn.pane.DataFrame(df, name=AllPlotTypes.dataframe_flat)]
 
     def dataframe_multi_index(self, pl_in: PlotInput) -> List[pn.panel]:
-        if self.match_all.matches(pl_in.plt_cnt_cfg):
-            df = pl_in.bench_cfg.ds.to_dataframe()
-            return [pn.pane.DataFrame(df, name=self.dataframe_multi_index.__name__)]
-        return []
+        """Returns a list of panel objects containing a multi-index dataframe."""
+        df = pl_in.bench_cfg.ds.to_dataframe()
+        return [pn.pane.DataFrame(df, name=AllPlotTypes.dataframe_multi_index)]
 
     def dataframe_mean(self, pl_in: PlotInput) -> List[pn.panel]:
-        if self.match_all.matches(pl_in.plt_cnt_cfg):
-            df = pl_in.bench_cfg.ds.mean("repeat").to_dataframe().reset_index()
-            return [pn.pane.DataFrame(df, name=self.dataframe_mean.__name__)]
-        return []
+        """Returns a list of panel objects containing a mean dataframe."""
+        df = pl_in.bench_cfg.ds.mean("repeat").to_dataframe().reset_index()
+        return [pn.pane.DataFrame(df, name=AllPlotTypes.dataframe_mean)]
 
     def xarray(self, pl_in: PlotInput) -> List[pn.panel]:
-        if self.match_all.matches(pl_in.plt_cnt_cfg):
-            return [pn.panel(pl_in.bench_cfg.ds, name=self.xarray.__name__)]
-        return []
+        """Returns a list of panel objects containing an xarray object."""
+        return [pn.panel(pl_in.bench_cfg.ds, name=AllPlotTypes.xarray)]
