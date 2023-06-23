e9750605c24be29790466484d75a32d30076f38d
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-06-23 17:14:30 +0100

    add method to add list of plots

--- bencher/plotting/plot_collection.py
+++ bencher/plotting/plot_collection.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 import panel as pn
+import inspect
+from typing import List
 
 from bencher.bench_cfg import PltCntCfg, BenchCfg
 from bencher.bench_vars import ParametrizedSweep
 from bencher.plotting.plot_filter import PlotProvider, PlotInput
-import inspect
-
 
 class PlotCollection:
     """A set of plot providers with filters that determine if they are able to plot the type of data in the results.  The plot collection can be customised with user defined plots or ones already defined by bencher. You can also take predefine PlotCollections defined in PlotLibrary and add or remove specific plots that you want to override.  The add and remove function accept strings, or StrEnums (which are equivalent to strings)"""
@@ -38,12 +38,25 @@ class PlotCollection:
             plot_name (str): The name of the plot to add.  This can either be a string, or from the StrEnum PlotTypes (which is also a string)
 
         Returns:
-            PlotCollection: Returns a reference to this class so that you can call with a fluent api, e.g plot_coll.add(barplot).remove(swarmplot)
+            PlotCollection: Returns a reference to this class so that you can call with a fluent api, e.g plot_coll.add("barplot").remove("swarmplot")
         """
         if plot_name in self.plotter_providers:
             self.plotters[plot_name] = self.plotter_providers[plot_name]
         else:
             raise ValueError("This plot was not found in the list of available plots")
+        return self
+
+    def add_list(self, plot_names: List[str]) -> PlotCollection:
+        """Add a list of plotting methods to the list of active plotting functions
+
+        Args:
+            plot_name (List[str]): A list of names of the plot to add. The list can contain either strings, or StrEnum PlotTypes (which is also a string)
+
+        Returns:
+            PlotCollection: Returns a reference to this class so that you can call with a fluent api, e.g plot_coll.add_list(["barplot","boxplot"]).remove("swarmplot")
+        """
+        for p in plot_names:
+            self.add(p)
         return self
 
     def remove(self, plot_name: str) -> PlotCollection:
