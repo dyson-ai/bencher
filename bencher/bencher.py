08ba10a8d8ffc5ada7bd4ea087a11f243baaea9b
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-06-24 14:21:12 +0000

    fixed pylint errors

--- bencher/bencher.py
+++ bencher/bencher.py
@@ -22,8 +22,7 @@ from bencher.bench_vars import (
 from bencher.plt_cfg import BenchPlotter
 from bencher.bench_cfg import BenchCfg, BenchRunCfg, DimsCfg
 from bencher.bench_plot_server import BenchPlotServer
-from bencher.plotting.plot_library import PlotLibrary
-from bencher.plotting.plot_types import PlotTypes
+from bencher.plotting.plot_collection import PlotCollection
 
 # Customize the formatter
 formatter = logging.Formatter("%(levelname)s: %(message)s")
@@ -99,7 +98,7 @@ class Bench(BenchPlotServer):
         bench_name: str = None,
         worker: Callable = None,
         worker_input_cfg: ParametrizedSweep = None,
-        plot_lib=None,
+        plot_lib: PlotCollection = None,
     ) -> None:
         """Create a new Bench object from a function and a class defining the inputs to the function
 
