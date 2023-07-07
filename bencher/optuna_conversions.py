ee364e42c0bf8f83ea4f91092e8a9c2b79200f3e
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-07-07 19:50:50 +0000

    remove extra display of parameter importance

--- bencher/optuna_conversions.py
+++ bencher/optuna_conversions.py
@@ -97,10 +97,12 @@ def collect_optuna_plots(bench_cfg: BenchCfg) -> List[pn.pane.panel]:
         if len(target_names) > 1:
             if len(target_names) <= 3:
                 # rows.append(plot_param_importances(study, target_name=target_names))
-                for tgt in target_names:
-                    rows.append(
-                        plot_param_importances(study, target=lambda t: t.values[0], target_name=tgt)
-                    )
+                # rows.append(target_names)
+
+                # for tgt in target_names:
+                #     rows.append(
+                #         plot_param_importances(study, target=lambda t: t.values[0], target_name=tgt)
+                #     )
 
                 rows.append(
                     plot_pareto_front(
@@ -118,6 +120,7 @@ def collect_optuna_plots(bench_cfg: BenchCfg) -> List[pn.pane.panel]:
                     )
                 )
             if bench_cfg.repeats > 1:
+                rows.append("repeats>1")
                 for tgt in target_names:
                     rows.append(
                         plot_param_importances(study, target=lambda t: t.values[0], target_name=tgt)
