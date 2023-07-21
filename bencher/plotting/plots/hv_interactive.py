197c61b46ee82246d23fa6288b2945181d6510b6
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-07-21 09:26:55 +0000

    fix linting

--- bencher/plotting/plots/hv_interactive.py
+++ bencher/plotting/plots/hv_interactive.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import panel as pn
 import hvplot.xarray  # noqa pylint: disable=unused-import
-import holoviews as hv
+import holoviews as hv  # noqa pylint: disable=unused-import
 
 from bencher.plotting.plot_filter import PlotFilter, PlotInput, VarRange
 from bencher.plotting.plot_types import PlotTypes  # noqa pylint: disable=unused-import
@@ -9,13 +9,6 @@ from bencher.plotting.plot_types import PlotTypes  # noqa pylint: disable=unused
 
 class HvInteractive:
     float_0_cat_at_least1_vec_1_res_1 = PlotFilter(
-        float_range=VarRange(0, 0),
-        cat_range=VarRange(1, None),
-        vector_len=VarRange(1, 1),
-        result_vars=VarRange(1, 1),
-    )
-
-    scatter_filter1 = PlotFilter(
         float_range=VarRange(0, 0),
         cat_range=VarRange(1, None),
         vector_len=VarRange(1, 1),
@@ -61,29 +54,29 @@ class HvInteractive:
 
         return None
 
-        return None
-        if self.scatter_filter.matches(pl_in.plt_cnt_cfg):
-            x = pl_in.plt_cnt_cfg.cat_vars[0]
-            y = pl_in.rv
-            ds = pl_in.bench_cfg.ds[pl_in.rv.name]
-            title = f"{x.name} vs {y.name}"
+        # return None
+        # if self.scatter_filter.matches(pl_in.plt_cnt_cfg):
+        #     x = pl_in.plt_cnt_cfg.cat_vars[0]
+        #     y = pl_in.rv
+        #     ds = pl_in.bench_cfg.ds[pl_in.rv.name]
+        #     title = f"{x.name} vs {y.name}"
 
-            if pl_in.plt_cnt_cfg.cat_cnt > 1:
-                c = pl_in.plt_cnt_cfg.cat_vars[1].name
-                ds = ds.set_index(c)
+        #     if pl_in.plt_cnt_cfg.cat_cnt > 1:
+        #         c = pl_in.plt_cnt_cfg.cat_vars[1].name
+        #         ds = ds.set_index(c)
 
-            pt = hv.Overlay()
+        #     pt = hv.Overlay()
 
-            pt *= ds.mean("repeat").hvplot(x=x.name, y=y.name, kind="bar")
-            for r in ds.coords["repeat"]:
-                pt *= ds.sel(repeat=r).hvplot(
-                    x=x.name, y=y.name, kind="scatter", rot=45, color="black"
-                )
-            # pt = pl_in.bench_cfg.to_points()
+        #     pt *= ds.mean("repeat").hvplot(x=x.name, y=y.name, kind="bar")
+        #     for r in ds.coords["repeat"]:
+        #         pt *= ds.sel(repeat=r).hvplot(
+        #             x=x.name, y=y.name, kind="scatter", rot=45, color="black"
+        #         )
+        #     # pt = pl_in.bench_cfg.to_points()
 
-            return pn.Column(pt.opts(height=600, title=title), name=PlotTypes.scatter_hv)
+        #     return pn.Column(pt.opts(height=600, title=title), name=PlotTypes.scatter_hv)
 
-        return None
+        # return None
 
     def lineplot_hv(self, pl_in: PlotInput) -> Optional[pn.panel]:
         if self.lineplot_filter.matches(pl_in.plt_cnt_cfg):
