74a14575b48c7fcdd5063afd511b8ba5599d58af
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-07-21 15:54:35 +0000

    autoformat

--- bencher/plotting/plots/surface.py
+++ bencher/plotting/plots/surface.py
@@ -65,7 +65,7 @@ class SurfacePlot:
             result_vars=VarRange(1, 1),
         ).matches(pl_in.plt_cnt_cfg):
             hv.extension("bokeh")
-            
+
             xr_cfg = plot_float_cnt_2(pl_in.plt_cnt_cfg, pl_in.rv, pl_in.bench_cfg.debug)
             hv.extension("plotly")
             bench_cfg = pl_in.bench_cfg
