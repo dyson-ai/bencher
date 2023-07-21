7230b893cc81cc19d004d3e26012dbd83035a78d
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-07-21 15:00:30 +0000

    finalise surface plots

--- bencher/plotting/plots/surface.py
+++ bencher/plotting/plots/surface.py
@@ -6,14 +6,46 @@ import logging
 import xarray as xr
 
 from bencher.bench_cfg import BenchCfg
-from bencher.bench_vars import ParametrizedSweep
+from bencher.bench_vars import ParametrizedSweep, ResultVar
 from bencher.plotting.plot_filter import PlotFilter, PlotInput, VarRange, PltCntCfg
 from bencher.plt_cfg import PltCfgBase, BenchPlotter
 from bencher.plotting.plot_types import PlotTypes
 
-from bencher.plotting_functions import wrap_long_time_labels, plot_surface_holo
+from bencher.plotting_functions import wrap_long_time_labels
 import holoviews as hv
 from holoviews import opts
+
+
+def plot_float_cnt_2(plt_cnt_cfg: PltCntCfg, rv: ResultVar, debug: bool) -> PltCfgBase:
+    """A function for determining the plot settings if there are 2 float variable and updates the PltCfgBase
+
+    Args:
+        sns_cfg (PltCfgBase): See PltCfgBase definition
+        plt_cnt_cfg (PltCntCfg): See PltCntCfg definition
+
+    Returns:
+        PltCfgBase: See PltCfgBase definition
+    """
+    xr_cfg = PltCfgBase()
+    if plt_cnt_cfg.float_cnt == 2:
+        logging.info(f"surface plot: {rv.name}")
+        xr_cfg.plot_callback_xra = xr.plot.plot
+        xr_cfg.x = plt_cnt_cfg.float_vars[0].name
+        xr_cfg.y = plt_cnt_cfg.float_vars[1].name
+        xr_cfg.xlabel = f"{xr_cfg.x} [{plt_cnt_cfg.float_vars[0].units}]"
+        xr_cfg.ylabel = f"{xr_cfg.y} [{plt_cnt_cfg.float_vars[1].units}]"
+        xr_cfg.zlabel = f"{rv.name} [{rv.units}]"
+        xr_cfg.title = f"{rv.name} vs ({xr_cfg.x} and {xr_cfg.y})"
+
+        if plt_cnt_cfg.cat_cnt >= 1:
+            logging.info("surface plot with 1 categorical")
+            xr_cfg.row = plt_cnt_cfg.cat_vars[0].name
+            xr_cfg.num_rows = len(plt_cnt_cfg.cat_vars[0].values(debug))
+        if plt_cnt_cfg.cat_cnt >= 2:
+            logging.info("surface plot with 2> categorical")
+            xr_cfg.col = plt_cnt_cfg.cat_vars[1].name
+            xr_cfg.num_cols = len(plt_cnt_cfg.cat_vars[1].values(debug))
+    return xr_cfg
 
 
 class SurfacePlot:
@@ -34,9 +66,7 @@ class SurfacePlot:
             vector_len=VarRange(1, 1),
             result_vars=VarRange(1, 1),
         ).matches(pl_in.plt_cnt_cfg):
-            xr_cfg = BenchPlotter.plot_float_cnt_2(
-                pl_in.plt_cnt_cfg, pl_in.rv, pl_in.bench_cfg.debug
-            )
+            xr_cfg = plot_float_cnt_2(pl_in.plt_cnt_cfg, pl_in.rv, pl_in.bench_cfg.debug)
             hv.extension("plotly")
             bench_cfg = pl_in.bench_cfg
             rv = pl_in.rv
