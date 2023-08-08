f11ec19465d5bb8c90305b36769800368c6cb4e5
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-08 12:00:02 +0000

    basic cached param example works

--- bencher/__init__.py
+++ bencher/__init__.py
@@ -1,4 +1,4 @@
-from .bencher import Bench, BenchCfg, BenchRunCfg, to_bench
+from .bencher import Bench, BenchCfg, BenchRunCfg
 from .plt_cfg import BenchPlotter, PltCfgBase
 from .example.benchmark_data import ExampleBenchCfgIn, ExampleBenchCfgOut, bench_function
 from .bench_plot_server import BenchPlotServer
@@ -10,4 +10,6 @@ from .plotting.plot_library import PlotLibrary, PlotTypes
 from .plotting.plot_filter import PlotInput, VarRange, PlotFilter
 from .utils import hmap_canonical_input, get_nearest_coords, make_namedtuple
 from .variables.parametrised_sweep import ParametrizedSweep
+from .caching import CachedParams
+
 from .optuna_conversions import to_optuna, summarise_study
---
+++ bencher/caching.py
@@ -0,0 +1,47 @@
+from diskcache import Cache
+
+from bencher.variables.parametrised_sweep import ParametrizedSweep
+from bencher.utils import hash_sha1
+import logging
+from sortedcontainers import SortedDict
+
+
+class CachedParams(ParametrizedSweep):
+    def __init__(self, clear_cache=True, cache_name="fcache", **params):
+        super().__init__(**params)
+
+        self.cache = Cache(f"cachedir/{cache_name}/sample_cache")
+        logging.info(f"cache dir{self.cache.directory}")
+        print(f"cache dir{self.cache.directory}")
+
+        if clear_cache:
+            self.cache.clear()
+
+    def kwargs_to_hash_key(self, **kwargs):
+        return tuple(SortedDict(kwargs).items())
+
+    def in_cache(self, **kwargs):
+        self.update_params_from_kwargs(**kwargs)
+        # print(self.get_inputs_as_dict())
+        inputs_key = self.kwargs_to_hash_key(**self.get_inputs_as_dict())
+        key = hash_sha1(inputs_key)
+        print(f"key:{key},value: {inputs_key}")
+        value = None
+        if key in self.cache:
+            value = self.cache[key]
+        return key, value
+
+    def return_cache(self, key: tuple) -> dict:
+        value = self.get_results_values_as_dict()
+
+        # print(value)
+        self.cache[key] = value
+        return value
+
+    def cache_wrap(self, func, **kwargs):
+        key, value = self.in_cache(**kwargs)
+
+        if value is None:
+            value = func(**kwargs)
+            self.cache[key] = value
+        return value
