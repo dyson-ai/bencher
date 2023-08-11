408ee71c25dfc7c7d917dfb65a4ea721d2e3f789
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-11 09:39:23 +0000

    update simple bool to test publishing

--- bencher/example/example_simple_bool.py
+++ bencher/example/example_simple_bool.py
@@ -23,7 +23,7 @@ def example_1D_bool(run_cfg: bch.BenchRunCfg) -> bch.Bench:
     )
 
     # here we sample the input variable theta and plot the value of output1. The (noisy) function is sampled 20 times so you can see the distribution
-    bencher.plot_sweep(
+    res = bencher.plot_sweep(
         title="Example 1D Categorical",
         input_vars=[ExampleBenchCfgIn.param.noisy],
         # result_vars=[ExampleBenchCfgOut.param.out_sin, ExampleBenchCfgOut.param.out_bool],
@@ -31,6 +31,7 @@ def example_1D_bool(run_cfg: bch.BenchRunCfg) -> bch.Bench:
         description=example_1D_bool.__doc__,
         run_cfg=run_cfg,
     )
+    bencher.append(res.to_bar())
 
     return bencher
 
@@ -40,5 +41,9 @@ if __name__ == "__main__":
     ex_run_cfg.repeats = 3
     ex_run_cfg.print_pandas = True
     ex_run_cfg.over_time = False
+    ex_run_cfg.auto_plot = False
 
-    example_1D_bool(ex_run_cfg).show()
+    b = example_1D_bool(ex_run_cfg)
+
+    b.publish("ssh://git@stash.dyson.global.corp:7999/las/bench_reports.git")
+    # b.save()
