ba308001ed0fb1cbb9aca1ca1989353b7190d7f4
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-11 14:00:15 +0000

    update to_grid to accept override values

--- bencher/bench_cfg.py
+++ bencher/bench_cfg.py
@@ -551,8 +551,9 @@ class BenchCfg(BenchRunCfg):
 
         return hv.DynamicMap(cb, kdims=kdims)
 
-    def to_grid(self):
-        inputs = self.inputs_as_str()
+    def to_grid(self, inputs=None):
+        if inputs is None:
+            inputs = self.inputs_as_str()
         if len(inputs) > 2:
             inputs = inputs[:2]
         return self.to_holomap().grid(inputs)
