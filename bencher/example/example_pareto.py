c59376a5cf68af226ade28b6538e9e713d5ca78e
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-22 13:35:49 +0000

    fix linting

--- bencher/example/example_pareto.py
+++ bencher/example/example_pareto.py
@@ -40,10 +40,10 @@ This is a slightly unusual way of doing pareto optimisation as we are not using 
 
 
 if __name__ == "__main__":
-    run_cfg = bch.BenchRunCfg()
-    run_cfg.repeats = 2
-    run_cfg.level = 2
-    example_pareto(run_cfg).show()
+    run_cfg_ex = bch.BenchRunCfg()
+    run_cfg_ex.repeats = 2
+    run_cfg_ex.level = 2
+    example_pareto(run_cfg_ex).show()
 
 
 # Joint 0 violates configuration limits -1.5707963267948966 <= -1.6676049014345424 <= 0.0
