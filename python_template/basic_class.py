1f72dbc758d1cf9034d212eaf693c71d1ee24e37
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-25 08:41:24 +0000

    update coverage to ignore ... for protocol

--- python_template/basic_class.py
+++ python_template/basic_class.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
-
+from typing import Protocol
 
 @dataclass
 class BasicClass:
     int_var: int = 0
+
+class BasicProtocol(Protocol):
+
+    def method(self):
+        ...
