bed39f28d1c99b43e6b5df40943febc11b9b3f50
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-25 08:46:33 +0000

    autoformatted and ruff --fix all code

--- python_template/basic_class.py
+++ python_template/basic_class.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from typing import Protocol
 
+
 @dataclass
 class BasicClass:
     int_var: int = 0
 
-class BasicProtocol(Protocol):
 
+class BasicProtocol(Protocol):
     def method(self):
         ...
