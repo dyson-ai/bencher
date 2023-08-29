1021f9fa4c5e5116150c0ffe68e7bf0324e586a1
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-29 13:24:28 +0000

    remove old tests

--- test/test_sweep_base.py
+++ test/test_sweep_base.py
@@ -136,48 +136,14 @@ class TestSweepBase(unittest.TestCase):
     @given(st.floats(min_value=0.1, allow_nan=False, allow_infinity=False))
     def test_levels_float(self, upper) -> None:
         var_float = bch.FloatSweep(bounds=(0, upper))
-
         self.sweep_up_to(var_float, float)
 
-    # @given(st.integers(min_value=0), st.integers(min_value=1))
+    # @given(st.integers(min_value=0), st.integers(min_value=1,max_value=10))
     # def test_levels_int(self, start, var_range):
     #     var_int = bch.IntSweep(default=start, bounds=(start, start + var_range))
     #     self.sweep_up_to(var_int, int, level=5)
 
-    # res_old = SWP.param.var_int.with_level(0)
-
-    # for i in range(1, level):
-    #     res = var_int.with_level(i)
-    #     new_vals = res.values()
-    #     print(res_old.values(), new_vals)
-    #     for i in res_old.values():
-    #         self.assertTrue(isinstance(i, int))
-    #         self.assertTrue(i in new_vals)
-    #         for n in new_vals:
-    #             print("\t", i == n)
-    #     res_old = res
-
-    # def test_levels_enum(self):
-    #     level = 5
-
-    #     # class E
-    #     class SWP(bch.ParametrizedSweep):
-    #         var_enum = EnumSweep(PostprocessFn)
-
-    #         result = ResultVar()
-
-    #     res_old = AllSweepVars.param.var_enum.with_level(0)
-
-    #     for i in range(1, level):
-    #         res = AllSweepVars.param.var_float.with_level(i)
-    #         new_vals = res.values()
-    #         print(res_old.values(), new_vals)
-    #         for i in res_old.values():
-    #             self.assertTrue(i in new_vals)
-    #             for n in new_vals:
-    #                 print("\t", i == n)
-    #         res_old = res
-
+  
 
 if __name__ == "__main__":
     # TestSweepBase().test_override_defaults()
