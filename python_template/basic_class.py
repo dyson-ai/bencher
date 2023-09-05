468c9fcae41b45d647d6b819596d61d8610b512d
Austin Gregg-Smith <blooop@gmail.com>
2023-09-05 20:10:13 +0000

    remove unused code

--- python_template/basic_class.py
+++ python_template/basic_class.py
@@ -1,12 +1,7 @@
 from dataclasses import dataclass
-from typing import Protocol
-
 
 @dataclass
 class BasicClass:
     int_var: int = 0
 
 
-class BasicProtocol(Protocol):
-    def method(self):
-        ...
