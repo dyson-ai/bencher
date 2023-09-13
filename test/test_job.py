151c49e0a64fca484d255b94f8f254799932f0b8
Austin Gregg-Smith <blooop@gmail.com>
2023-09-13 19:06:39 +0000

    dispy

--- test/test_job.py
+++ test/test_job.py
@@ -4,7 +4,7 @@ import random
 from bencher.job import JobFunctionCache, JobCache, Job
 
 from hypothesis import given, strategies as st, settings
-
+import dispy
 
 class CachedParamExample(bch.ParametrizedSweep):
     var1 = bch.FloatSweep(default=0, bounds=[0, 100000])
@@ -117,19 +117,48 @@ def wrapper(**kwargs):
     # cp = CachedParamExample()  # clears cache by default
     var1 = kwargs.get("var1", 0)
     print(f"starting {var1}")
-    bp = BasicParam()
+    # bp = BasicParam()
     for i in range(1000):
         logging.debug(i)
 
     print(f"finishing {var1}")
 
     result = var1 + random.uniform(0, 1)
+    return 1
     return dict(result=result)
     # res = cp.__call__(**kwargs)
     # return res
 
 
+
+def run_dispy():
+    # executed on client only; variables created below, including modules imported,
+    # are not available in job computations
+    import dispy, random
+    # distribute 'compute' to nodes; in this case, 'compute' does not have
+    # any dependencies to run on nodes
+    # cluster = dispy.JobCluster(wrapper,depends=["/workspaces/bencher/test/test_job.py"])
+    cluster = dispy.JobCluster(wrapper,depends=[param,BasicParam])
+    # run 'compute' with 20 random numbers on available CPUs
+    jobs = []
+    for i in range(20):
+        job = cluster.submit(var1=random.randint(5,20))
+        jobs.append(job)
+    # cluster.wait() # waits until all jobs finish
+    for job in jobs:
+        result = job() # waits for job to finish and returns results
+
+        print(result)
+        # print('%s executed job %s at %s with %s' % (host, job.id, job.start_time))
+        # other fields of 'job' that may be useful:
+        # job.stdout, job.stderr, job.exception, job.ip_addr, job.end_time
+    cluster.print_status()  # shows which nodes executed how many jobs etc.
+
 if __name__ == "__main__":
+    import os
+    print(os.path.join(os.path.dirname(dispy.__file__), 'data'))
+    run_dispy()
+    exit()
 
     location = "cachedir/joblib"
     memory = Memory(location, verbose=0)
