25966d1dba9095615bc376f7dcd6bea4331001f9
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-09-14 14:40:23 +0000

    remove unused

--- test/run_dispy.py
+++ test/run_dispy.py
@@ -1,5 +1,6 @@
 # from typing import Any
 import dispy
+
 # import param
 import random
 import numpy as np
@@ -16,46 +17,41 @@ from bencher.example.benchmark_data import AllSweepVars
 # https://pycos.org/dispycos.html#docker-container
 # http://dispy.org/dispynode.html#containers
 
+
 def wrapper(**kwargs):
     from bencher.example.benchmark_data import AllSweepVars
+
     return AllSweepVars().__call__(**kwargs)
 
-if __name__ == '__main__':
+
+def dispy_wrapper(**kwargs):
+    
+
+if __name__ == "__main__":
     import random, dispy
-    # 'compute' needs definition of class C, so include in 'depends'
-    # cluster = dispy.JobCluster(C.__call__, depends=[C,param,param.Parameterized])
 
-    # param_deps = [param,param.parameterized,param.parameterized.serializer]
-    # bench_deps =[bencher,bencher.bencher,bencher.worker_job,bencher.utils,bencher.bench_cfg]
-    # func_deps = [bch.example.benchmark_data.AllSweepVars]
     # func_deps = [BasicParam,AllSweepVars]
-    # func_deps =[]
-    # depends =  func_deps+param_deps
-    # depends = param_deps
-    depends =[]
-    # depends =["/home/agsmith/bencher/test/run_dispy.py","/home/agsmith/bencher/test/basic_param.py"]
-    # depends =  param_deps+bench_deps +func_deps
-    cluster = dispy.JobCluster(wrapper,nodes=["10.50.103.17","10.50.103.16","10.51.103.13"], depends=depends)
-    # cluster = dispy.JobCluster(wrapper_bench,nodes=["10.50.103.17"], depends=depends,cleanup=False)
-
+    depends = []
+    nodes = []
+    #  nodes=["10.50.103.17", "10.50.103.16", "10.51.103.13"]
+    cluster = dispy.JobCluster(wrapper, nodes=nodes, depends=depends)
 
     run_cfg = bch.BenchRunCfg
-    bench = bch.Bench("all_vars",wrapper)
-    bench.plot_sweep("remote",[AllSweepVars.param.var_float])
-    bench.report.show()
+    bench = bch.Bench("all_vars", wrapper)
+    bench.plot_sweep("remote", [AllSweepVars.param.var_float])
+    # bench.report.show()
 
     jobs = []
-    for i in range(3):
+    for i in range(20):
         # c = C(i, random.uniform(1, 3)) # create object of C
         # job = cluster.submit(c) # it is sent to a node for executing 'compute'
 
-        job = cluster.submit(var_float=i/100.) # it is sent to a node for executing 'compute'
-
+        job = cluster.submit(var_float=i / 100.0)  # it is sent to a node for executing 'compute'
 
         # job.id = c # store this object for later use
         jobs.append(job)
     for job in jobs:
-        res =job() # wait for job to finish
+        res = job()  # wait for job to finish
         if res is not None:
             print(res)
         else:
@@ -67,13 +63,6 @@ if __name__ == '__main__':
                 logging.warning(job.stderr)
             if len(job.exception):
                 logging.critical(f"exception: { job.exception}")
-            
 
-
-
-
-        
         # print(res.result)
         # print(f"{job.result} : {job.stdout}")
-
-
