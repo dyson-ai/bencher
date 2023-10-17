8057e2399f457ca096f48f3632a50e1b7ca7a72d
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-10-17 13:47:59 +0000

    convert to a list

--- scripts/gen_dependencies_yaml.py
+++ scripts/gen_dependencies_yaml.py
@@ -9,11 +9,11 @@ with open("pyproject.toml", encoding="utf-8") as f:
 deps_yaml = ["pip:"]
 
 for dep in pyproject_dict["project"]["optional-dependencies"]["test"]:
-    deps_yaml.append(f"    test: {dep}")
+    deps_yaml.append(f"    - test: {dep}")
 
 deps_yaml.append("")
 for dep in pyproject_dict["project"]["dependencies"]:
-    deps_yaml.append(f"    buildrun: {dep}")
+    deps_yaml.append(f"    - buildrun: {dep}")
 
 with open("dependencies.yaml", "w", encoding="utf-8") as f:
     for dep in deps_yaml:
