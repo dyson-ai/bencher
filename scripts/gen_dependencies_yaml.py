033cb162089d8b3eaa018fb92da14353c9523eeb
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-10-17 13:38:53 +0000

    update name

--- scripts/gen_dependencies_yaml.py
+++ scripts/gen_dependencies_yaml.py
@@ -1,17 +1,18 @@
-#takes a pyproject.toml files and generates a dependencies.yaml file.
+# takes a pyproject.toml files and generates a dependencies.yaml file.
 import tomli
-with open("pyproject.toml",encoding="utf-8") as f:
-    deps = tomli.loads(f.read())
 
-deps_yaml=["pip:"]
+with open("pyproject.toml", encoding="utf-8") as f:
+    pyproject_dict = tomli.loads(f.read())
 
-for dep in deps["project"]["optional-dependencies"]["test"]:
+deps_yaml = ["pip:"]
+
+for dep in pyproject_dict["project"]["optional-dependencies"]["test"]:
     deps_yaml.append(f"    test: {dep}")
 
 deps_yaml.append("")
-for dep in deps["project"]["dependencies"]:
+for dep in pyproject_dict["project"]["dependencies"]:
     deps_yaml.append(f"    buildrun: {dep}")
 
-with open("dependencies.yaml", 'w',encoding="utf-8") as f:
+with open("dependencies.yaml", "w", encoding="utf-8") as f:
     for dep in deps_yaml:
         f.write(f"{dep}\n")
