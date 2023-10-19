61bc2edb8c00eb62442b35b6d90768429edbdd11
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-10-19 09:30:01 +0000

    comment out cone code

--- bencher/example/example_float3D.py
+++ bencher/example/example_float3D.py
@@ -95,15 +95,15 @@ def example_floats3D(
 
     bench.report.append(res.to_volume())
 
-    bench.plot_sweep(
-        title="Float 3D Cone Example",
-        input_vars=[VolumeSample.param.x, VolumeSample.param.y, VolumeSample.param.z],
-        result_vars=[
-            VolumeResult.param.interesting_vec,
-            VolumeResult.param.interesting_vec_and_occ,
-        ],
-        description="""This example shows how to sample 3 floating point variables and plot a 3D vector field of the results.""",
-    )
+    # bench.plot_sweep(
+    #     title="Float 3D Cone Example",
+    #     input_vars=[VolumeSample.param.x, VolumeSample.param.y, VolumeSample.param.z],
+    #     result_vars=[
+    #         VolumeResult.param.interesting_vec,
+    #         VolumeResult.param.interesting_vec_and_occ,
+    #     ],
+    #     description="""This example shows how to sample 3 floating point variables and plot a 3D vector field of the results.""",
+    # )
 
     return bench
 
