ae9a311529712686d9048be9e1cc2408a6739ec3
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-11-27 15:04:04 +0000

    autoformat

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'bencher'
-copyright = '2023, Austin Gregg-Smith'
-author = 'Austin Gregg-Smith'
-release = '0.3.9'
+project = "bencher"
+copyright = "2023, Austin Gregg-Smith"  # pylint:disable=redefined-builtin
+author = "Austin Gregg-Smith"
+release = "0.3.9"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
