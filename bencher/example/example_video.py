3b8ec46c16f179fabf6693432877a5efddb4571f
Austin Gregg-Smith <blooop@gmail.com>
2023-12-13 10:22:49 +0000

    move into a class

--- bencher/example/example_video.py
+++ bencher/example/example_video.py
@@ -1,27 +1,61 @@
 import bencher as bch
-
-
-class TuringPattern(bch.ParametrizedSweep):
-    pass
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = 2.8e-4
-b = 5e-3
-tau = 0.1
-k = -0.005
 
-size = 100  # size of the 2D grid
-dx = 2.0 / size  # space step
-T = 9.0  # total time
-dt = 0.001  # time step
-n = int(T / dt)  # number of iterations
+# from https://ipython-books.github.io/124-simulating-a-partial-differential-equation-reaction-diffusion-systems-and-turing-patterns/
+class TuringPattern(bch.ParametrizedSweep):
+    pass
 
+    a = 2.8e-4
+    b = 5e-3
+    tau = 0.1
+    k = -0.005
 
-U = np.random.rand(size, size)
-V = np.random.rand(size, size)
+    size = 100  # size of the 2D grid
+    dx = 2.0 / size  # space step
+    T = 9.0  # total time
+    dt = 0.001  # time step
+    n = int(T / dt)  # number of iterations
+
+    def __call__(self, **kwargs):
+        self.update_params_from_kwargs(**kwargs)
+
+        U = np.random.rand(self.size, self.size)
+        V = np.random.rand(self.size, self.size)
+
+        fig, axes = plt.subplots(3, 3, figsize=(8, 8))
+        step_plot = n // 9
+        # We simulate the PDE with the finite difference
+        # method.
+        for i in range(n):
+            # We compute the Laplacian of u and v.
+            deltaU = laplacian(U)
+            deltaV = laplacian(V)
+            # We take the values of u and v inside the grid.
+            Uc = U[1:-1, 1:-1]
+            Vc = V[1:-1, 1:-1]
+            # We update the variables.
+            U[1:-1, 1:-1], V[1:-1, 1:-1] = (
+                Uc + dt * (a * deltaU + Uc - Uc**3 - Vc + k),
+                Vc + dt * (b * deltaV + Uc - Vc) / tau,
+            )
+            # Neumann conditions: derivatives at the edges
+            # are null.
+            for Z in (U, V):
+                Z[0, :] = Z[1, :]
+                Z[-1, :] = Z[-2, :]
+                Z[:, 0] = Z[:, 1]
+                Z[:, -1] = Z[:, -2]
+
+            # We plot the state of the system at
+            # 9 different times.
+            if i % step_plot == 0 and i < 9 * step_plot:
+                ax = axes.flat[i // step_plot]
+                show_patterns(U, ax=ax)
+                ax.set_title(f"$t={i * dt:.2f}$")
+
+        return super().__call__()
 
 
 def laplacian(Z):
@@ -36,38 +70,6 @@ def laplacian(Z):
 def show_patterns(U, ax=None):
     ax.imshow(U, cmap=plt.cm.copper, interpolation="bilinear", extent=[-1, 1, -1, 1])
     ax.set_axis_off()
-
-
-fig, axes = plt.subplots(3, 3, figsize=(8, 8))
-step_plot = n // 9
-# We simulate the PDE with the finite difference
-# method.
-for i in range(n):
-    # We compute the Laplacian of u and v.
-    deltaU = laplacian(U)
-    deltaV = laplacian(V)
-    # We take the values of u and v inside the grid.
-    Uc = U[1:-1, 1:-1]
-    Vc = V[1:-1, 1:-1]
-    # We update the variables.
-    U[1:-1, 1:-1], V[1:-1, 1:-1] = (
-        Uc + dt * (a * deltaU + Uc - Uc**3 - Vc + k),
-        Vc + dt * (b * deltaV + Uc - Vc) / tau,
-    )
-    # Neumann conditions: derivatives at the edges
-    # are null.
-    for Z in (U, V):
-        Z[0, :] = Z[1, :]
-        Z[-1, :] = Z[-2, :]
-        Z[:, 0] = Z[:, 1]
-        Z[:, -1] = Z[:, -2]
-
-    # We plot the state of the system at
-    # 9 different times.
-    if i % step_plot == 0 and i < 9 * step_plot:
-        ax = axes.flat[i // step_plot]
-        show_patterns(U, ax=ax)
-        ax.set_title(f"$t={i * dt:.2f}$")
 
 
 plt.show()
