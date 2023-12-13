65bd75c44e22a7df5ae151e2667125dead9fa258
Austin Gregg-Smith <blooop@gmail.com>
2023-12-13 09:46:04 +0000

    delete example files

--- bencher/results/md_ex.py
+++
@@ -1,9 +0,0 @@
-import panel as pn
-
-markdown_text = "This is a Markdown pane with a blue background."
-markdown_pane = pn.pane.Markdown(
-    markdown_text, style={"background-color": "blue", "color": "white"}
-)
-
-app = pn.Column(markdown_pane)
-app.show()
