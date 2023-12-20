0b05ca0593ed4e75cc7e37615f69f4aa81dede01
Austin Gregg-Smith <blooop@gmail.com>
2023-12-20 11:06:58 +0000

    add to_panes_multi

--- bencher/results/panel_result.py
+++ bencher/results/panel_result.py
@@ -78,6 +78,19 @@ class PanelResult(BenchResultBase):
             return self.map_plots(partial(self.to_panes_single, container=container, **kwargs))
         return matches_res.to_panel()
 
+    def to_panes_multi(
+        self, plot_callback, result_var: ResultVar = None, target_dimension=1, **kwargs
+    ) -> Optional[pn.pane.panel]:
+        if result_var is None:
+            result_var = self.bench_cfg.result_vars[0]
+        xr_dataarray = self.to_dataarray(result_var)
+        return self._to_panes_da(
+            xr_dataarray,
+            plot_callback=plot_callback,
+            target_dimension=target_dimension,
+            **kwargs,
+        )
+
     def to_panes_single(
         self, result_var: ResultVar, container=pn.pane.panel, **kwargs
     ) -> Optional[pn.pane.panel]:
@@ -168,6 +181,72 @@ class PanelResult(BenchResultBase):
             else:
                 for val in da.values:
                     inner.append(container(val, **kwargs))
+            outer_container.append(inner)
+
+        return outer_container
+
+    def _to_panes_da(
+        self,
+        da: xr.DataArray,
+        plot_callback=pn.pane.panel,
+        in_card=True,
+        target_dimension=1,
+        **kwargs,
+    ) -> pn.panel:
+        num_dims = len(da.dims)
+        if num_dims > target_dimension:
+            dim_sel = da.dims[-1]
+
+            dim_color = color_tuple_to_css(int_to_col(num_dims - 2, 0.05, 1.0))
+
+            background_col = dim_color
+            name = " vs ".join(da.dims)
+            outer_container = pn.Card(
+                title=name,
+                name=name,
+                styles={"background": background_col},
+                header_background=dim_color,
+            )
+
+            # todo remove this pre calculation and let panel work out the right sizes
+            padded_labels = []
+            sliced_da = []
+            max_label_size = 0
+            for i in range(da.sizes[dim_sel]):
+                sliced = da.isel({dim_sel: i})
+                padded_labels.append(
+                    f"{dim_sel}={sliced.coords[dim_sel].values}",
+                )
+                label_size = len(padded_labels[-1])
+                if label_size > max_label_size:
+                    max_label_size = label_size
+                sliced_da.append(sliced)
+
+            for i in range(da.sizes[dim_sel]):
+                sliced = sliced_da[i]
+
+                panes = self._to_panes_da(
+                    sliced,
+                    plot_callback=plot_callback,
+                    in_card=False,
+                    target_dimension=target_dimension,
+                )
+                label = padded_labels[i].rjust(max_label_size, " ")
+                side = pn.pane.Markdown(
+                    f"{label}", align=("end", "center"), width=max_label_size * 8
+                )
+
+                outer_container.append(pn.Row(side, panes))
+        else:
+            name = f"{da.dims[0]} vs {da.name}"
+            if in_card:
+                outer_container = pn.Card(title=name, name=name)
+            else:
+                outer_container = pn.Column(name=name)
+            outer_container= pn.Row(name=name)
+
+            inner = pn.Row(styles={"background": "white"})
+            inner.append(plot_callback(da=da, **kwargs))
             outer_container.append(inner)
 
         return outer_container
