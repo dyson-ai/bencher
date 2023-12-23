f3dc62f614e44c64725daba2222dde719ab502f2
Austin Gregg-Smith <blooop@gmail.com>
2023-12-23 11:41:18 +0000

    add example_meta cat and float

---
+++ bencher/example/example_meta_cat.py
@@ -0,0 +1,24 @@
+import bencher as bch
+from bencher.example.example_meta import BenchMeta
+
+
+def example_meta_levels(
+    run_cfg: bch.BenchRunCfg = bch.BenchRunCfg(), report: bch.BenchReport = bch.BenchReport()
+) -> bch.Bench:
+    bench = bch.Bench("bench_meta", BenchMeta(), report=report, run_cfg=run_cfg)
+
+    bench.plot_sweep(
+        title="Meta Bench Floats",
+        description="This uses bencher to show how bencher plots sweeps of categorical variables of increasing dimensionality",
+        input_vars=[BenchMeta.param.categorical_vars.with_sample_values([1, 2, 3])],
+        const_vars=[
+            BenchMeta.param.float_vars.with_const(0),
+            BenchMeta.param.level.with_const(4),
+        ],
+    )
+
+    return bench
+
+
+if __name__ == "__main__":
+    example_meta_levels().report.show()
