d6b44c216a468f74fc6fd990ca3e0f67444aa188
Austin Gregg-Smith <blooop@gmail.com>
2024-01-07 13:12:28 +0000

    add working example

---
+++ bencher/example/taichi/working.py
@@ -0,0 +1,13 @@
+import bencher as bch
+from bencher.example.taichi.turing_pattern import SweepTuring
+
+if __name__ == "__main__":
+    run_cfg = bch.BenchRunCfg()
+    run_cfg.level = 2
+    run_cfg.use_sample_cache = False
+    bench = SweepTuring().to_bench(run_cfg)
+
+    SweepTuring.param.Du.bounds = [0.13, 0.19]
+
+    bench.plot_sweep("turing", input_vars=[SweepTuring.param.feed])
+    bench.report.show()
