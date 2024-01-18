c223c655722c63f5154dad7e99254eb2bd2d4b26
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2024-01-18 10:38:02 +0000

    fix colcon and setuptools incompatibilities

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools"],
+    # install_requires=["setuptools"],
     zip_safe=False,
     test_suite="test",
     tests_require=["pytest"],
