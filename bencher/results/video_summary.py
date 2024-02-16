eac3461b47e5a3acca2c28525b1a949abe4ca9c6
Austin Gregg-Smith <blooop@gmail.com>
2024-02-16 17:40:08 +0000

    comment out to_video_summary ds

--- bencher/results/video_summary.py
+++ bencher/results/video_summary.py
@@ -28,65 +28,68 @@ class VideoSummaryResult(BenchResultBase):
         result_types=(ResultImage,),
         **kwargs,
     ) -> Optional[pn.panel]:
-        plot_filter = PlotFilter(
-            float_range=VarRange(0, None),
-            cat_range=VarRange(0, None),
-            panel_range=VarRange(1, None),
+        return self.to_video_grid(
+            result_var=result_var, result_types=result_types, time_sequence_dimension=-1, **kwargs
         )
-        matches_res = plot_filter.matches_result(
-            self.plt_cnt_cfg, callable_name(self.to_video_summary_ds)
-        )
+        # plot_filter = PlotFilter(
+        #     float_range=VarRange(0, None),
+        #     cat_range=VarRange(0, None),
+        #     panel_range=VarRange(1, None),
+        # )
+        # matches_res = plot_filter.matches_result(
+        #     self.plt_cnt_cfg, callable_name(self.to_video_summary_ds)
+        # )
 
-        # video_controls = VideoControls()
-        if matches_res.overall:
-            ds = self.to_dataset(ReduceType.SQUEEZE)
-            row = pn.Row()
-            for rv in self.get_results_var_list(result_var):
-                if isinstance(rv, result_types):
-                    row.append(self.to_video_summary_ds(ds, rv, input_order, reverse, **kwargs))
-            return row
-        return matches_res.to_panel()
+        # # video_controls = VideoControls()
+        # if matches_res.overall:
+        #     ds = self.to_dataset(ReduceType.SQUEEZE)
+        #     row = pn.Row()
+        #     for rv in self.get_results_var_list(result_var):
+        #         if isinstance(rv, result_types):
+        #             row.append(self.to_video_summary_ds(ds, rv, input_order, reverse, **kwargs))
+        #     return row
+        # return matches_res.to_panel()
 
-    def to_video_summary_ds(
-        self,
-        dataset: xr.Dataset,
-        result_var: Parameter,
-        input_order: List[str] = None,
-        reverse: bool = True,
-        video_controls: VideoControls = None,
-        **kwargs,
-    ):
-        vr = VideoWriter()
-        da = dataset[result_var.name]
+    # def to_video_summary_ds(
+    #     self,
+    #     dataset: xr.Dataset,
+    #     result_var: Parameter,
+    #     input_order: List[str] = None,
+    #     reverse: bool = True,
+    #     video_controls: VideoControls = None,
+    #     **kwargs,
+    # ):
+    #     vr = VideoWriter()
+    #     da = dataset[result_var.name]
 
-        if input_order is None:
-            input_order = list(da.dims)
-        else:
-            input_order = params_to_str(input_order)
-        if reverse:
-            input_order = list(reversed(input_order))
+    #     if input_order is None:
+    #         input_order = list(da.dims)
+    #     else:
+    #         input_order = params_to_str(input_order)
+    #     if reverse:
+    #         input_order = list(reversed(input_order))
 
-        inputs_produc = [da.coords[i].values for i in input_order]
+    #     inputs_produc = [da.coords[i].values for i in input_order]
 
-        for index in itertools.product(*inputs_produc):
-            lookup = dict(zip(input_order, index))
-            val = da.loc[lookup].item()
-            index = listify(index)
-            for i in range(len(index)):
-                if isinstance(index[i], (int, float)):
-                    index[i] = FormatFloat()(index[i])
-            label = ", ".join(f"{a[0]}={a[1]}" for a in list(zip(input_order, index)))
-            if val is not None:
-                vr.append_file(val, label)
-        fn = vr.write_png(**kwargs)
-        kwargs.pop("target_duration", None)
-        if fn is not None:
-            if video_controls is None:
-                video_controls = VideoControls()
-            vid = video_controls.video_container(fn, **kwargs)
-            return vid
+    #     for index in itertools.product(*inputs_produc):
+    #         lookup = dict(zip(input_order, index))
+    #         val = da.loc[lookup].item()
+    #         index = listify(index)
+    #         for i in range(len(index)):
+    #             if isinstance(index[i], (int, float)):
+    #                 index[i] = FormatFloat()(index[i])
+    #         label = ", ".join(f"{a[0]}={a[1]}" for a in list(zip(input_order, index)))
+    #         if val is not None:
+    #             vr.append_file(val, label)
+    #     fn = vr.write_png(**kwargs)
+    #     kwargs.pop("target_duration", None)
+    #     if fn is not None:
+    #         if video_controls is None:
+    #             video_controls = VideoControls()
+    #         vid = video_controls.video_container(fn, **kwargs)
+    #         return vid
 
-        return None
+    #     return None
 
     def to_video_grid(
         self,
