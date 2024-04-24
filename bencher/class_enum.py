65e7088454432d5dc61a4c13b5d2450ec8e2680f
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2024-04-24 08:38:10 +0000

    wip on generic class enum

--- bencher/class_enum.py
+++ bencher/class_enum.py
@@ -10,8 +10,8 @@ from enum import auto
 class ClassEnum(StrEnum):
     """A ClassEnum is a pattern to make it easier to create factory a factory method that converts from an enum to a corresponding class.  Subclasses should implement to_class(enum_instance:EnumType) which takes an enum returns the corresponding instance of that class."""
 
-    @classmethod
-    def to_class_generic(cls, module_import: str, class_name: str) -> Any:
+    @staticmethod
+    def to_class_generic(module_import: str, class_name: str) -> Any:
         """Create an instance of the class referred to by this enum
 
         Returns:
@@ -21,11 +21,14 @@ class ClassEnum(StrEnum):
         class_def = getattr(importlib.import_module(module_import), class_name)
         return class_def()
 
-    @classmethod
     @abstractmethod
-    def to_class(cls, enum_val: ClassEnum) -> Any:
-        """Subclasses should overrides this method to take  an enum returns the corresponding instance of that class."""
+    def to_class(self):
+        """Subclasses should overrides this method to return the corresponding instance of that class."""
         raise NotImplementedError()
+
+    @classmethod
+    def to_class_from_enum(cls, enum_val: ClassEnum) -> Any:
+        return enum_val.to_class()
 
 
 @dataclass
@@ -47,6 +50,5 @@ class ExampleEnum(ClassEnum):
     Class1 = auto()
     Class2 = auto()
 
-    @classmethod
-    def to_class(cls, enum_val: ExampleEnum) -> BaseClass:
-        return cls.to_class_generic("bencher.class_enum", enum_val)
+    def to_class(self) -> BaseClass:
+        return ClassEnum.to_class_generic("bencher.class_enum", self)
--- test/test_class_enum.py
+++ test/test_class_enum.py
@@ -3,8 +3,14 @@ import bencher as bch
 
 
 class TestClassEnum(unittest.TestCase):
-    def test_basic(self):
-        instance1 = bch.ExampleEnum.to_class(bch.ExampleEnum.Class1)
+    def test_to_class(self):
+        instance1 = bch.ExampleEnum.Class1.to_class()
         self.assertEqual(instance1.classname, "class1")
-        instance2 = bch.ExampleEnum.to_class(bch.ExampleEnum.Class2)
+        instance2 = bch.ExampleEnum.Class2.to_class()
+        self.assertEqual(instance2.classname, "class2")
+
+    def test_to_class_from_enum(self):
+        instance1 = bch.ExampleEnum.to_class_from_enum(bch.ExampleEnum.Class1)
+        self.assertEqual(instance1.classname, "class1")
+        instance2 = bch.ExampleEnum.to_class_from_enum(bch.ExampleEnum.Class2)
         self.assertEqual(instance2.classname, "class2")
