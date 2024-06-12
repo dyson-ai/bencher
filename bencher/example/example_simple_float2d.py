0b51fe0a176045154d2f9400df4ae173d6b585a6
Austin Gregg-Smith <blooop@gmail.com>
2024-06-12 13:48:43 +0100

    update simple float examples

--- bencher/example/example_simple_float2d.py
+++ bencher/example/example_simple_float2d.py
@@ -1,7 +1,6 @@
 """This file has some examples for how to perform basic benchmarking parameter sweeps"""
 
 import math
-import random
 import bencher as bch
 
 
@@ -9,15 +8,16 @@ class SimpleFloat(bch.ParametrizedSweep):
     theta = bch.FloatSweep(
         default=0, bounds=[0, math.pi], doc="Input angle", units="rad", samples=30
     )
+    offset = bch.FloatSweep(default=0, bounds=[0, 1], doc="Input angle", units="rad")
     out_sin = bch.ResultVar(units="v", doc="sin of theta")
 
     def __call__(self, **kwargs):
         self.update_params_from_kwargs(**kwargs)
-        self.out_sin = math.sin(self.theta)
+        self.out_sin = math.sin(self.theta) + self.offset
         return super().__call__(**kwargs)
 
 
-def example_1D_float(run_cfg: bch.BenchRunCfg = None, report: bch.BenchReport = None) -> bch.Bench:
+def example_2D_float(run_cfg: bch.BenchRunCfg = None, report: bch.BenchReport = None) -> bch.Bench:
     """This example shows how to sample a 1 dimensional float variable and plot the result of passing that parameter sweep to the benchmarking function"""
 
     bench = SimpleFloat().to_bench(run_cfg, report)
@@ -26,4 +26,4 @@ def example_1D_float(run_cfg: bch.BenchRunCfg = None, report: bch.BenchReport = 
 
 
 if __name__ == "__main__":
-    example_1D_float().report.show()
+    example_2D_float().report.show()
--- test/test_bench_examples.py
+++ test/test_bench_examples.py
@@ -4,6 +4,7 @@ from bencher.example.example_floats2D import example_floats2D
 from bencher.example.example_pareto import example_pareto
 from bencher.example.example_simple_cat import example_1D_cat
 from bencher.example.example_simple_float import example_1D_float
+from bencher.example.example_simple_float2d import example_2D_float
 from bencher.example.example_float_cat import run_example_float_cat
 from bencher.example.example_time_event import run_example_time_event
 from bencher.example.example_float3D import example_floats3D
@@ -74,6 +75,12 @@ class TestBenchExamples(unittest.TestCase):
 
     def test_example_simple_float(self) -> None:
         self.examples_asserts(example_1D_float(self.create_run_cfg()))
+
+    def test_example_simple_float1D(self) -> None:
+        self.examples_asserts(example_1D_float(self.create_run_cfg()))
+
+    def test_example_simple_float2D(self) -> None:
+        self.examples_asserts(example_2D_float(self.create_run_cfg()))
 
     def test_example_float_cat(self) -> None:
         self.examples_asserts(run_example_float_cat(self.create_run_cfg()))
