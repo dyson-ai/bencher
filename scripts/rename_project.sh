46c4a7fa8279932b57c93ed09063c670a4a9dc7c
JustaGist <saifksidhik@gmail.com>
2024-07-30 09:41:06 +0100

    add author name and email changes possible with project renaming task

--- scripts/rename_project.sh
+++ scripts/rename_project.sh
@@ -3,4 +3,14 @@
 mv python_template "$1"
 mv python_template.deps.yaml "$1".deps.yaml
 
+# change project name in all files
 find . \( -type d -name .git -prune \) -o \( -type f -not -name 'tasks.json' -not -name 'update_from_template.sh' -not -name 'update_from_template_ours.sh' \) -print0 | xargs -0 sed -i "s/python_template/$1/g"
+
+# author name
+find . \( -type d -name .git -prune \) -o \( -type f -not -name 'tasks.json' -not -name 'update_from_template.sh' -not -name 'update_from_template_ours.sh' \) -print0 | xargs -0 sed -i "s/Austin Gregg-Smith/$2/g"
+
+# author email
+find . \( -type d -name .git -prune \) -o \( -type f -not -name 'tasks.json' -not -name 'update_from_template.sh' -not -name 'update_from_template_ours.sh' \) -print0 | xargs -0 sed -i "s/blooop@gmail.com/$3/g"
+
+# github username
+find . \( -type d -name .git -prune \) -o \( -type f -not -name 'setup.host' -not -name 'update_from_template.sh' -not -name 'update_from_template_ours.sh' \) -print0 | xargs -0 sed -i "s/blooop/$4/g"
