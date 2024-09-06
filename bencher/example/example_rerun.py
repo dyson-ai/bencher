7b738f04534aa75e175f027882ce978b6ea9d049
Austin Gregg-Smith <blooop@gmail.com>
2024-09-06 16:36:36 +0100

    clean up example

--- bencher/example/example_rerun.py
+++ bencher/example/example_rerun.py
@@ -2,7 +2,6 @@ import bencher as bch
 import rerun as rr
 import rerun.blueprint as rrb
 
-
 my_blueprint = rrb.Blueprint(
     rrb.Horizontal(
         rrb.BarChartView(),
@@ -13,27 +12,21 @@ my_blueprint = rrb.Blueprint(
     ),
 )
 
-
-# rr.init("rerun_example_my_blueprint", spawn=True, default_blueprint=my_blueprint)
-
-
 rr.init(
     "rerun_example_my_blueprint",
-    # spawn=True,
+    spawn=True
 )
 
 
 file_path = "dat.rrd"
 rr.save(file_path)
 
-rr.send_blueprint(my_blueprint, make_active=True)
+# rr.send_blueprint(my_blueprint, make_active=True)
 rr.log("s1", rr.Scalar(1))
 rr.log("s1", rr.Scalar(2))
 rr.log("s1", rr.Scalar(1))
 rr.log("s1", rr.Scalar(2))
 
-
-# exit()
 bch.publish_and_view_rrd(
     file_path,
     remote="https://github.com/dyson-ai/bencher.git",
