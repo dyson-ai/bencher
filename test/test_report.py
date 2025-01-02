07890e40f8e3d23ef1ae151989027ccf45a59f11
Austin Gregg-Smith <blooop@gmail.com>
2025-01-02 20:28:10 +0000

    update pixi.lock

--- test/test_report.py
+++ test/test_report.py
@@ -36,6 +36,5 @@ import os
 class TestReport(unittest.TestCase):
     """The purpose of this test class is to run the example problems to make sure they don't crash.  The bencher logic is tested in the other test files test_bencher.py and test_vars.py"""
 
-
     def test_example_floats2D_report(self) -> None:
-        example_floats2D(bch.BenchRunCfg(level=2).report.save()
+        example_floats2D(bch.BenchRunCfg(level=2)).report.save()
