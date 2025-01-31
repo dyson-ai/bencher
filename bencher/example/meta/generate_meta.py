5361f8bb4733e7046113c06c882d644ef6319635
Austin Gregg-Smith <blooop@gmail.com>
2025-01-31 15:26:23 +0000

    fix merge

--- bencher/example/meta/generate_meta.py
+++ bencher/example/meta/generate_meta.py
@@ -96,11 +96,7 @@ res.to_auto_plots()
         ]
         from pathlib import Path
 
-<<<<<<< HEAD
-        fname = Path(f"docs/reference/meta/ex_{title}.ipynb")
-=======
         fname = Path(f"docs/reference/Meta/ex_{title}.ipynb")
->>>>>>> feature/scrolling
         fname.parent.mkdir(parents=True, exist_ok=True)
         fname.write_text(nbf.writes(nb), encoding="utf-8")
 
