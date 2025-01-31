5913ee8143eeaded109df0026a8b63b388c28449
Austin Gregg-Smith <blooop@gmail.com>
2025-01-31 08:43:36 +0000

    reduce level to 4

--- bencher/example/meta/generate_meta.py
+++ bencher/example/meta/generate_meta.py
@@ -74,6 +74,7 @@ from bencher.example.meta.example_meta import BenchableObject
 
 run_cfg = bch.BenchRunCfg()
 run_cfg.repeats = {self.sample_with_repeats}
+run_cfg.level = 4 
 bench = BenchableObject().to_bench(run_cfg)
 res=bench.plot_sweep(input_vars={input_vars},
                     result_vars=["distance","sample_noise"])
