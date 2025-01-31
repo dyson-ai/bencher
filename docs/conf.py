3b37308636d6e308dd3c933cf30ef3ff532f65ff
Austin Gregg-Smith <blooop@gmail.com>
2025-01-31 13:30:52 +0000

    turn off sticky sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -35,12 +35,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # html_theme = "pydata_sphinx_theme"
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "sticky_navigation": True,  # Keeps the sidebar static while scrolling
-}
+# html_theme_options = {
+#     "sticky_navigation": True,  # Keeps the sidebar static while scrolling
+# }
 html_static_path = ["_static"]
 html_css_files = [
-    "custom.css",
+    "custom.css"
 ]
 
 autoapi_dirs = ["../bencher"]
