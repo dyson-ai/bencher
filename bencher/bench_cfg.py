ba2d6fadc309e2009acea7c8e549e3ac604c27ca
Austin Gregg-Smith <blooop@gmail.com>
2025-02-14 15:52:20 +0000

    remove full

--- bencher/bench_cfg.py
+++ bencher/bench_cfg.py
@@ -434,7 +434,7 @@ class BenchCfg(BenchRunCfg):
             if title:
                 name = self.title
             else:
-                name = "Full Data Collection Parameters"
+                name = "Data Collection Parameters"
         col = pn.Column(name=name)
         if title:
             col.append(self.to_title())
